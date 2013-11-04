93793168a3efd0f5f8f9a5df68a11013135994d9
thejoshlamb <joshlamb@gmail.com>
2013-11-04 14:03:47 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imijur::Application.config.secret_key_base = '6a2905416e029bebbef79c747575ebef02bd29453e05e84213f5cc6815a6f8ad09879ea16f52729cb76a865ab404cd9f2e2150a61bed5a39e1fc354cd5ae1e5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imijur::Application.config.session_store :cookie_store, key: '_imijur_session'
